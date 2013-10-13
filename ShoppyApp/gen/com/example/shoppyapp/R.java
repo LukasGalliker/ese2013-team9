ed8da58665680fd409014841d04c056f0974f39a
Sebastien Broggi <sebi.broggi@gmail.com>
2013-10-13 18:02:42 +0200

    Created Welcomescreen
    Designed Welcomescreen and added Function for creating User-DB-Entry and
    then jump to DisplayListsActivity
    Added colors.xml for colors

--- ShoppyApp/gen/com/example/shoppyapp/R.java
+++ ShoppyApp/gen/com/example/shoppyapp/R.java
@@ -10,6 +10,10 @@ package com.example.shoppyapp;
 public final class R {
     public static final class attr {
     }
+    public static final class color {
+        public static final int gray=0x7f040000;
+        public static final int white=0x7f040001;
+    }
     public static final class dimen {
         /**  Default screen margins, per the Android Design guidelines. 
 
@@ -17,25 +21,39 @@ public final class R {
          screen margins) for sw720dp devices (e.g. 10" tablets) in landscape here.
     
          */
-        public static final int activity_horizontal_margin=0x7f040000;
-        public static final int activity_vertical_margin=0x7f040001;
+        public static final int activity_horizontal_margin=0x7f050000;
+        public static final int activity_vertical_margin=0x7f050001;
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int action_settings=0x7f090005;
+        public static final int button1=0x7f090003;
+        public static final int nickname=0x7f090002;
+        public static final int pager=0x7f090000;
+        public static final int section_label=0x7f090004;
+        public static final int textView1=0x7f090001;
     }
     public static final class layout {
-        public static final int activity_welcome_screen=0x7f030000;
+        public static final int activity_display_lists=0x7f030000;
+        public static final int activity_welcome_screen=0x7f030001;
+        public static final int fragment_display_lists_dummy=0x7f030002;
     }
     public static final class menu {
-        public static final int welcome_screen=0x7f070000;
+        public static final int display_lists=0x7f080000;
+        public static final int welcome_screen=0x7f080001;
     }
     public static final class string {
-        public static final int action_settings=0x7f050001;
-        public static final int app_name=0x7f050000;
-        public static final int welcome=0x7f050002;
+        public static final int action_settings=0x7f060001;
+        public static final int app_name=0x7f060000;
+        public static final int nickname=0x7f060005;
+        public static final int save=0x7f060004;
+        public static final int title_activity_display_lists=0x7f060003;
+        public static final int title_section1=0x7f060006;
+        public static final int title_section2=0x7f060007;
+        public static final int title_section3=0x7f060008;
+        public static final int welcome=0x7f060002;
     }
     public static final class style {
         /** 
@@ -59,10 +77,10 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f060000;
+        public static final int AppBaseTheme=0x7f070000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f060001;
+        public static final int AppTheme=0x7f070001;
     }
 }
--- ShoppyApp/src/com/example/shoppyapp/WelcomeScreen.java
+++ ShoppyApp/src/com/example/shoppyapp/WelcomeScreen.java
@@ -1,20 +1,18 @@
 package com.example.shoppyapp;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.widget.EditText;
 
 public class WelcomeScreen extends Activity {
-	
+	public final static String EXTRA_MESSAGE = "com.example.myfirstapp.NICKNAME";
     @Override
     public void onCreate (Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome_screen);
-        
-        // example for usage of the local database
-        LocalDatabaseHandler db = new LocalDatabaseHandler(this);
-        db.addUser(new User(42, "Mehldau", "fj3fjh"));
-        String nameOfUser = db.getUser(42).name;
     }
 
 	@Override
@@ -22,6 +20,20 @@ public class WelcomeScreen extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.welcome_screen, menu);
 		return true;
+	}
+	
+	//Called when clicking on "Save"-Button
+	public void createDatabase(View view){
+	    Intent intent = new Intent(this, DisplayListsActivity.class);
+	    EditText editText = (EditText) findViewById(R.id.nickname);
+	    String nickname = editText.getText().toString();
+	    
+	    //Add Entry in DB
+        LocalDatabaseHandler db = new LocalDatabaseHandler(this);
+        db.addUser(new User(1, nickname, "fj3fjh"));
+        //Switch to DisplayListsActivity
+	    intent.putExtra(EXTRA_MESSAGE, nickname);
+	    startActivity(intent);
 	}
 
 }
