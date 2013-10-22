471e806026c829499b9d955089424136f45ab66d
Marc Schneiter <schneiter@eclipso.ch>
2013-10-23 00:11:12 +0200

    cleared own dir

--- ShoppyApp/gen/com/eseteam9/shoppyapp/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package com.eseteam9.shoppyapp;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
--- ShoppyApp/src/com/eseteam9/shoppyapp/AbstractHandler.java
+++
@@ -1,9 +0,0 @@
-package com.eseteam9.shoppyapp;
-
-public abstract class AbstractHandler {
-	protected static New_LocalDatabaseHandler dbHandler;
-	
-	public static void setHandler(New_LocalDatabaseHandler dbHandler) {
-		AbstractHandler.dbHandler = dbHandler;
-	}
-}
